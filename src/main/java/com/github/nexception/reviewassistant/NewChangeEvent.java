0a23e085aaf75bbb22ff676d81a91591c15fd2e8
Simon Wessel <simon.w.karlsson@gmail.com>
2014-11-07 11:34:31 +0100

    Add ChangeEvent listener
    Change-Id: Ibe33f5b44f27a87fd714736c785c5df7fa85a011

---
+++ src/main/java/com/github/nexception/reviewassistant/NewChangeEvent.java
@@ -0,0 +1,17 @@
+package com.github.nexception.reviewassistant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+class NewChangeEvent implements ChangeListener {
+
+    private static final Logger log = LoggerFactory.getLogger(NewChangeEvent.class);
+
+    @Override
+    public void stateChanged(ChangeEvent changeEvent) {
+
+    }
+}
