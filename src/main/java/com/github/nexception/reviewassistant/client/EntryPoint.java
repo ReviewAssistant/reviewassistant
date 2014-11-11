1257bf101c0076b6ff1dbd3565d2597e0680b830
Simon Wessel <simon.w.karlsson@gmail.com>
2014-11-11 12:30:37 +0100

    A test with GWT ui etension.
    Change-Id: I1283d8a8e75136116d92bc36385f15ec1864223e

---
+++ src/main/java/com/github/nexception/reviewassistant/client/EntryPoint.java
@@ -0,0 +1,11 @@
+package com.github.nexception.reviewassistant.client;
+
+import com.google.gerrit.plugin.client.PluginEntryPoint;
+
+public class EntryPoint extends PluginEntryPoint {
+
+    @Override
+    public void onPluginLoad() {
+
+    }
+}
