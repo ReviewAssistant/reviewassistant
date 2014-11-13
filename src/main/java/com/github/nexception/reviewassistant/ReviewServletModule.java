66176315ad2c4836c67dd5c0fb24f14de0899b13
Gustav Jansson Ekstrand <gustav.jp@live.se>
2014-11-13 16:31:09 +0100

    reviewassistant.js now prints to the change_plugins div and does a POST to /print Added ReviewServlet and ReviewServletModule
    Change-Id: I51f4a7377e1fe11c4fd0589f15e9000e57782101

---
+++ src/main/java/com/github/nexception/reviewassistant/ReviewServletModule.java
@@ -0,0 +1,10 @@
+package com.github.nexception.reviewassistant;
+
+import com.google.inject.servlet.ServletModule;
+
+public class ReviewServletModule extends ServletModule {
+    @Override
+    protected void configureServlets() {
+        super.configureServlets();
+    }
+}
