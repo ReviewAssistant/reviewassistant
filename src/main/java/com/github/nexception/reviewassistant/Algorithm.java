89896e4055e2e6bc30cd56074f058bf473df1bab
Gustav Jansson Ekstrand <gustav.jp@live.se>
2014-11-21 10:01:36 +0100

    Added method for calculating review sessions
    Change-Id: I9e21cef96fffe0774a203602407da9d4a4404319

--- src/main/java/com/github/nexception/reviewassistant/Algorithm.java
+++ src/main/java/com/github/nexception/reviewassistant/Algorithm.java
@@ -24,6 +24,7 @@ public class Algorithm {
         Calculation calculation = new Calculation();
         calculation.commitId = event.patchSet.revision;
         calculation.totalReviewTime = calculateReviewTime(event);
+        calculation.sessions = calculateReviewSessions(calculateReviewTime(event));
 
         return calculation;
     }
@@ -32,6 +33,7 @@ public class Algorithm {
      * Returns the total amount of time in minutes recommended for a review.
      * Adds all line insertions and deletions for a patch set and calculates
      * the amount of minutes needed.
+     * This calculation is based on the optimum review rate of 5 LOC in 1 minute.
      * @param event the event for a patch set
      * @return      the total amount of time recommended for a review
      */
@@ -39,11 +41,25 @@ public class Algorithm {
         int lines = event.patchSet.sizeInsertions - event.patchSet.sizeDeletions;
         log.info("Insertions: " + event.patchSet.sizeInsertions);
         log.info("Deletions: " + event.patchSet.sizeDeletions);
-        int minutes = (int) (lines * 0.2);
+        int minutes = lines / 5;
         if(minutes < 5) {
             minutes = 5;
         }
         return minutes;
+    }
+
+    /**
+     * Returns the recommended amount of review sessions for a review.
+     * Divides the total amount of review time up in 60 minute sessions.
+     * @param minutes the total amount of time recommended for a review
+     * @return        the recommended amount of review sessions
+     */
+    private static int calculateReviewSessions(int minutes) {
+        int sessions = Math.round(minutes / 60);
+        if(sessions < 1) {
+            sessions = 1;
+        }
+        return sessions;
     }
 }
 
