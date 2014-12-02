3aed78e3a3d95ab137cc93c8dedcdebd2235bc34
Gustav Jansson Ekstrand <gustav.jp@live.se>
2014-12-02 14:50:48 +0100

    Changed the text formatting of the advice.
    Change-Id: Idbbe8b595bb7f8ec08fef00dc9720ef4a560b516

--- src/main/java/com/github/nexception/reviewassistant/rest/GetAdvice.java
+++ src/main/java/com/github/nexception/reviewassistant/rest/GetAdvice.java
@@ -8,11 +8,13 @@ import com.google.gerrit.extensions.restapi.ResourceConflictException;
 import com.google.gerrit.extensions.restapi.RestReadView;
 import com.google.gerrit.server.change.RevisionResource;
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 
 /**
  * This rest view fetches a calculation and returns it. It is used by the front-end to
  * present the review suggestions to the users.
  */
+@Singleton
 public class GetAdvice implements RestReadView<RevisionResource> {
 
     private Storage storage;
@@ -25,19 +27,22 @@ public class GetAdvice implements RestReadView<RevisionResource> {
     @Override
     public Object apply(RevisionResource resource) throws AuthException, BadRequestException, ResourceConflictException {
         Calculation calculation = storage.fetchCalculation(resource.getPatchSet().getRevision().get());
-        String advice = "<div>Reviewers should spend ";
+        String advice = "<div><p></p><b>ReviewAssistant</b>";
+        advice += "<div>Reviewers should spend <b>";
         try {
-            if (calculation.hourBlocks > 0) {
-                advice += calculation.hourBlocks + " hours and ";
+            if (calculation.hourBlocks == 1) {
+                advice += calculation.hourBlocks + " hour</b> and <b>";
+            } else if(calculation.hourBlocks > 1) {
+                advice += calculation.hourBlocks + " hours</b> and <b>";
             }
-            advice += (calculation.fiveMinuteBlocks * 5) + " minutes reviewing this change.</div>";
+            advice += (calculation.fiveMinuteBlocks * 5) + " minutes</b> reviewing this change.</div>";
             if (calculation.sessions > 1) {
-                advice += "<div>This could be split up in " + calculation.sessions + " sessions.</div>";
+                advice += "<div>This should be split up in <b>" + calculation.sessions + " sessions</b>.</div>";
             }
         } catch (NullPointerException e) {
             advice = "<div>No advice exists for this change.</div>";
         }
-
+        advice += "</div>";
         return advice;
     }
 }
