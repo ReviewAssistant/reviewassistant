4b56a43346e4eea68f50c29050563baaa6bfa37f
William Phan <william.da.phan@gmail.com>
2015-01-05 17:50:56 +0100

    Removed change owner from query
    Change-Id: If945fb989a04658116a263b11f6273dd48da2fe7

--- src/main/java/com/github/nexception/reviewassistant/ReviewAssistant.java
+++ src/main/java/com/github/nexception/reviewassistant/ReviewAssistant.java
@@ -153,7 +153,9 @@ public class ReviewAssistant implements Runnable {
         Map<Account, Integer> reviewersApproved = new HashMap<>();
         try {
             //TODO: Move limit to config
-            List<ChangeInfo> infoList = gApi.changes().query("status:merged -age:8weeks limit:10 label:Code-Review=2 project:" + projectName.toString()).withOptions(ListChangesOption.LABELS, ListChangesOption.DETAILED_ACCOUNTS).get();
+            List<ChangeInfo> infoList = gApi.changes().query("status:merged -owner:" + change.getOwner().get() +
+                    " -age:8weeks limit:10 label:Code-Review=2 project:" +
+                    projectName.toString()).withOptions(ListChangesOption.LABELS, ListChangesOption.DETAILED_ACCOUNTS).get();
             for (ChangeInfo info : infoList) {
                 log.info(info.labels.get("Code-Review").approved.username);
                 Account account = accountCache.getByUsername(info.labels.get("Code-Review").approved.username).getAccount();
