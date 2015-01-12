2f4ec17d16d7421853da33680daa594d961f7040
William Phan <william.da.phan@gmail.com>
2015-01-12 14:56:00 +0100

    Moved reviewTimeModifier to cache
    Change-Id: I508e1ac3d228467ecbb9056416e37e8d295713d7

--- src/main/java/com/github/nexception/reviewassistant/SimpleCache.java
+++ src/main/java/com/github/nexception/reviewassistant/SimpleCache.java
@@ -7,6 +7,8 @@ import com.google.gerrit.extensions.api.changes.ChangeApi;
 import com.google.gerrit.extensions.common.ChangeInfo;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.server.change.RevisionResource;
+import com.google.gerrit.server.config.PluginConfigFactory;
+import com.google.gerrit.server.project.NoSuchProjectException;
 import com.google.gson.Gson;
 import com.google.inject.Inject;
 import org.slf4j.Logger;
@@ -30,19 +32,24 @@ public class SimpleCache implements Cache {
     private static final Logger log = LoggerFactory.getLogger(SimpleCache.class);
     private File dir;
     private GerritApi gApi;
+    private PluginConfigFactory cfg;
+    private double reviewTimeModifier;
 
-    @Inject
-    SimpleCache(@PluginData File dir, GerritApi gApi) {
+    @Inject SimpleCache(@PluginData File dir, GerritApi gApi, PluginConfigFactory cfg) {
         this.dir = dir;
         this.gApi = gApi;
+        this.cfg = cfg;
     }
 
     @Override
     public void storeCalculation(Calculation calculation) {
-        File file = new File(dir, calculation.commitId.substring(0, 2) + File.separator + calculation.commitId.substring(2));
+        File file = new File(dir,
+            calculation.commitId.substring(0, 2) + File.separator + calculation.commitId
+                .substring(2));
         log.debug("Writing calculation to {}", file);
         file.getParentFile().mkdirs();
-        try (BufferedWriter writer = Files.newBufferedWriter(file.toPath(), Charset.forName("UTF-8"))) {
+        try (BufferedWriter writer = Files
+            .newBufferedWriter(file.toPath(), Charset.forName("UTF-8"))) {
             Gson gson = new Gson();
             String s = gson.toJson(calculation);
             writer.write(s, 0, s.length());
@@ -58,26 +65,42 @@ public class SimpleCache implements Cache {
 
     @Override
     public Calculation fetchCalculation(RevisionResource resource) {
-        File file = new File(dir, resource.getPatchSet().getRevision().get().substring(0, 2) + File.separator + resource.getPatchSet().getRevision().get().substring(2));
+        File file = new File(dir,
+            resource.getPatchSet().getRevision().get().substring(0, 2) + File.separator + resource
+                .getPatchSet().getRevision().get().substring(2));
         Calculation calculation = null;
         log.debug("Loading calculation from {}", file);
-        try (BufferedReader reader = Files.newBufferedReader(file.toPath(), Charset.forName("UTF-8"))) {
+        try (BufferedReader reader = Files
+            .newBufferedReader(file.toPath(), Charset.forName("UTF-8"))) {
             Gson gson = new Gson();
             calculation = gson.fromJson(reader.readLine(), Calculation.class);
             log.info("Returning Calculation {}", calculation.toString());
         } catch (IOException e) {
-            log.error("Could not read calculation file for {}", resource.getPatchSet().getRevision().get());
+            log.error("Could not read calculation file for {}",
+                resource.getPatchSet().getRevision().get());
             log.error(e.toString());
         }
-        if(calculation == null || calculation.totalReviewTime == 0) {
-            log.debug("Corrupt or missing calculation. Will recalculate for {}", resource.getPatchSet().getRevision().get());
+
+        try {
+            reviewTimeModifier =
+                cfg.getProjectPluginConfigWithInheritance(resource.getChange().getProject(),
+                    "reviewassistant")
+                    .getInt("time", "reviewTimeModifier", 100);
+        } catch (NoSuchProjectException e) {
+            log.error(e.getMessage(), e);
+        }
+
+        if (calculation == null || calculation.totalReviewTime == 0) {
+            log.debug("Corrupt or missing calculation. Will recalculate for {}",
+                resource.getPatchSet().getRevision().get());
             try {
                 ChangeApi cApi = gApi.changes().id(resource.getChange().getChangeId());
                 ChangeInfo info = cApi.get();
-                calculation = ReviewAssistant.calculate(info);
+                calculation = ReviewAssistant.calculate(info, reviewTimeModifier/100);
                 storeCalculation(calculation);
             } catch (RestApiException e) {
-                log.error("Could not get ChangeInfo for change {}", resource.getChange().getChangeId());
+                log.error("Could not get ChangeInfo for change {}",
+                    resource.getChange().getChangeId());
             }
         }
         return calculation;
