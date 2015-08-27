367f89d37e7a183df99e349efd16b431a3ff1c5d
David Pursehouse <david.pursehouse@sonymobile.com>
2015-08-27 15:47:38 +0900

    Adapt to moved location of FactoryModule
    In the latest master branch of Gerrit, the FactoryModule is moved
    into the extensions package.
    
    Change-Id: Iaaf8acd26013782a0f197f7166fd5345bc9963b5

--- src/main/java/com/github/reviewassistant/reviewassistant/Module.java
+++ src/main/java/com/github/reviewassistant/reviewassistant/Module.java
@@ -2,9 +2,9 @@ package com.github.reviewassistant.reviewassistant;
 
 import com.github.reviewassistant.reviewassistant.rest.GetAdvice;
 import com.google.gerrit.common.EventListener;
+import com.google.gerrit.extensions.config.FactoryModule;
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.extensions.restapi.RestApiModule;
-import com.google.gerrit.server.config.FactoryModule;
 
 import static com.google.gerrit.server.change.RevisionResource.REVISION_KIND;
 
